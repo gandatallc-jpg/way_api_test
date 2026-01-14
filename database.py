3ea0cfdb74d0aa02294a24180e78421ae80ceeba
gandatallc-jpg <gandatallc@gmail.com>
2026-01-14 21:43:41 +0800

    sad

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
+asdsa
 DB_PASS = os.getenv("DB_PASS")
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
